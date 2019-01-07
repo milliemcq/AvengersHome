266c61be30477bec396f735b65fbe8a5c0f76d79
Your Name Here <millie@mcquillin.com>
2019-01-07 15:20:25 +0000

    new authentication system

--- index.js
+++ index.js
@@ -116,14 +116,67 @@ app.get('/people/:username', function(req, resp) {
     resp.send(response);
 });
 
-app.post('/api/posts', verifyToken, (req, res) => {
+app.get('/api', function api(req, res) {
     res.json({
-        message: 'Post Created.........'
+        description: 'My API. Please authenticate!'
     });
 });
 
 app.post('/api/login', (req, res) => {
-    jwt.sign({admin}, 'secretkey', (err, token) => {
+
+    // insert code here to actually authenticate, or fake it
+    const user = { id: 3 };
+
+    // then return a token, secret key should be an env variable
+    const token = jwt.sign({ user: user.id }, 'secret_key_goes_here');
+    res.json({
+        message: 'Authenticated! Use this token in the "Authorization" header',
+        token: token
+    });
+});
+
+app.get('/api/protected', ensureToken, (req, res) => {
+    jwt.verify(req.token, 'secret_key_goes_here', function(err, data) {
+        if (err) {
+            res.sendStatus(403);
+        } else {
+            res.json({
+                description: 'Protected information. Congrats!'
+            });
+        }
+    });
+});
+
+function ensureToken(req, res, next) {
+    const bearerHeader = req.headers["authorization"];
+    if (typeof bearerHeader !== 'undefined') {
+        const bearer = bearerHeader.split(" ");
+        const bearerToken = bearer[1];
+        req.token = bearerToken;
+        next();
+    } else {
+        res.sendStatus(403);
+    }
+}
+
+
+/*
+app.post('/api/posts', verifyToken, (req, res) => {
+    jwt.verify(req.token, 'secretkey', (err, authData) => {
+        if (err) {
+            res.sendStatus(403);
+        } else {
+            res.json({
+                message: 'Post created...',
+                authData
+            });
+        }
+        ;
+    });
+});
+
+app.post('/api/login', (req, res) => {
+    jwt.sign({admin}, 'secretkey', {expiresIn: '10h'} (err, token) => {
         res.json({
             token
         });
@@ -140,12 +193,19 @@ app.get('/api', (req, res) => {
 function verifyToken(req, res, next){
     const bearerHeader = req.headers['authoization'];
     if(typeof bearerHeader !== 'undefined'){
-
+        // Split at the space
+        const bearer = bearerHeader.split(' ');
+        // Get token from array
+        const bearerToken = bearer[1];
+        // Set the token
+        req.token = bearerToken;
+        // Next middleware
+        next();
     }
     else{
         console.log("forbidden");
         res.sendStatus(403);
     }
-}
+}*/
 
 app.listen(8090)
